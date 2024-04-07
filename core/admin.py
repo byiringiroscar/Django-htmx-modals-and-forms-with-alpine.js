ce896e0e34ae448f222806e089efb8f05f0e58db
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-07 14:06:31 +0200

    Clean and admin structue

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from core.models import Complaint
 
 # Register your models here.
+
+
+admin.site.register(Complaint)
--- core/forms.py
+++ core/forms.py
@@ -4,6 +4,13 @@ from core.models import Complaint
 
 
 class ComplaintForm(forms.ModelForm):
+
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if email.startswith('b'):
+            raise forms.ValidationError("No emails beginning with 'b' allowed.")
+        return email
     class Meta:
         model = Complaint
         fields = ("text", "email")
